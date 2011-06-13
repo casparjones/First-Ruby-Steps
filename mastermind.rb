90fd69d7deb0603c021185c86c50cf3632a62c84
casparjones <frank@vlatten.biz>
2011-06-14 01:37:08 +0200

    fixed some mastermind problem

--- mastermind.rb
+++ mastermind.rb
@@ -1,8 +1,10 @@
+#!/usr/bin/ruby
 
 class Mastermind
 
   def initialize
     @zahl = [rand(5) + 1, rand(5) + 1, rand(5) + 1, rand(5) + 1]
+    puts @zahl
     puts "Starte Mastermind";
     puts "Anleitung:
 Suche den Code. Der Code besteht aus 4 Zahlen von 1 - 6.
@@ -12,18 +14,18 @@ Du hast 8 Versuche um den Code zu knacken!
 #### <- gesuchter Code!"
   end
 
-  def checkZahl(zahl, stelle)
-    if(stelle == 1)
+  def checkZahlOk(zahl)
+    if(@prufzahl == nil)
       @prufzahl = @zahl.dup
     end
-
-    if(@zahl[stelle - 1] == zahl)
-      return 2
-    end
-
-    @prufzahl.each { |z|
+    
+    puts @prufzahl
+    @prufzahl.each_index { |index|
+      z = @prufzahl.at(index)
+      # puts "prüfen #{z} == #{zahl}"
       if(zahl == z)
-        @prufzahl.delete(z)
+        #puts "ok"
+        @prufzahl.delete_at(index)
         return 1
       end
     }
@@ -31,24 +33,76 @@ Du hast 8 Versuche um den Code zu knacken!
     return false
   end
 
-  def gibTip(tip)
-    stelle = 0
-    result = "";
+  def checkZahlPositionOk(zahl, stelle)
+    if(@prufzahl == nil)
+      @prufzahl = @zahl.dup
+    end    
+    puts "#{zahl} stelle #{stelle} #{@zahl[stelle]} == #{zahl}"
+    if(@zahl[stelle] == zahl)
+      @prufzahl.delete_at(stelle)
+      zahl = 0
+      return 2
+    end
+    return false
+  end
+
+  def getZeichen(zahlresult)
+    if(zahlresult == 2)
+      result = "•"
+    end
+    if(zahlresult == 1)
+      result = "⊗"
+    end
+    
+    return result
+  end
+
+  def checkZahl(tip)
+    result = ""
     tip.each_char {|c|
-      stelle = stelle + 1
       zahl = c.to_i
-      zahlresult = checkZahl(zahl, stelle)
-      if(zahlresult && zahlresult > 0)
-        if(zahlresult == 2)
-          result = result + "•"
-        end
-        if(zahlresult == 1)
-          result = result + "⊗"
+      if(zahl > 0 && zahl < 7)
+        zahlresult = checkZahlOk(zahl)
+        if(zahlresult && zahlresult > 0)
+          result = result + getZeichen(zahlresult)
         end
       end
     }
+    return result
+  end
 
+  def checkZahlPosition(tip)
+    @tip = tip.split(//)
+    result = ""
+    stelle = 0
+    puts @tip
+    @tip.each_index {|index|
+      c = @tip.at(index)
+      puts "zahl #{c}"
+      zahl = c.to_i
+      if(zahl>0 && zahl<7)
+        zahlresult = checkZahlPositionOk(zahl, stelle)
+        if(zahlresult && zahlresult > 0)
+          @tip.delete_at(index)
+          result = result + getZeichen(zahlresult)
+        end
+        stelle = stelle + 1
+      end
+    }
+    @tip = @tip.to_s    
+    return result
+  end
+
+  def gibTip(tip)
+    stelle = 0
+    result = "";
+    result1 = checkZahlPosition(tip)
+    puts @prufzahl.to_s
+    result2 = checkZahl(@tip)
+    puts "tip: #{@tip}"
+    result  = result1 + result2
     puts "#{tip.chop!} " + result
+    @prufzahl = nil
     return result
   end
 
