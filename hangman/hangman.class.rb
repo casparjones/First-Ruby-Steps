959ae4e8ccf82a0c04e83fda38e2441a1fae1302
casparjones <frank@vlatten.biz>
2011-06-16 00:00:05 +0200

    added the 'hangman' and lives to the hangman game

--- hangman/hangman.class.rb
+++ hangman/hangman.class.rb
@@ -18,6 +18,68 @@ class Hangman
 		#puts @buchstaben
 	end
 	
+	def drawHangman(deadStatus) 
+		case deadStatus.to_i
+		when 0 then
+			puts '_____   '
+			puts '|   |   '
+			puts '|   0   '
+			puts '|  /|\  '
+			puts '|  / \  '
+			puts '|       '
+			puts '|\      '
+		when 1 then
+			puts '_____   '
+			puts '|   |   '
+			puts '|   0   '
+			puts '|  /|\  '
+			puts '|       '
+			puts '|       '
+			puts '|\      '
+		when 2 then
+			puts '_____   '
+			puts '|   |   '
+			puts '|   0   '
+			puts '|       '
+			puts '|       '
+			puts '|       '
+			puts '|\      '
+		when 3 then
+			puts '_____   '
+			puts '|   |   '
+			puts '|       '
+			puts '|       '
+			puts '|       '
+			puts '|       '
+			puts '|\      '
+		when 4 then
+			puts '_____   '
+			puts '|       '
+			puts '|       '
+			puts '|       '
+			puts '|       '
+			puts '|       '
+			puts '|\      '
+		when 5 then
+			puts '        '
+			puts '|       '
+			puts '|       '
+			puts '|       '
+			puts '|       '
+			puts '|       '
+			puts '|\      '
+		when 6 then
+			puts '        '
+			puts '        '
+			puts '        '
+			puts '        '
+			puts '        '
+			puts '|       '
+			puts '|\      '
+		end
+
+	end
+	
 	def loadWort
 		db = SQLite3::Database.new("woerterliste.db")
 		anzahlWorter = db.execute('SELECT count(*) FROM woerter').to_s.to_i
@@ -43,21 +105,32 @@ class Hangman
 	end
 	
 	def check_buchstabe(buchstabe)
+		found = false
 		@buchstaben.each_with_index { |char,index| 
 			if(char.to_s.downcase == buchstabe.chop.downcase)
 				@gitter[index] = char
+				found = true
 			end
 		}
+		return found
 	end
 	
 	def start_game()
+		leben = 7
 		self.draw_fields
 		loop do
 			buchstabe = user_input()
 			if(buchstabe.chop == 'help') 
 				puts "Immer dieses fuschen! Gesuchte Wort: " + @wort
 			else
-				check_buchstabe(buchstabe)
+				found = check_buchstabe(buchstabe)
+				if(found == false)
+					leben = leben - 1
+					drawHangman(leben)
+					if(leben == 0)
+						puts "Du bist leider tot -.- Das Gesuchte Wort war '#{@wort}'"; break;
+					end
+				end
 			end
 			
 			draw_fields
